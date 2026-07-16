81c2ff091508fd06fdb677d337b562f8af8b9019
Omar Itani <omar.itaniau@gmail.com>
2026-07-16 16:21:31 +1000

    Initial commit: Hello World program in C++

---
+++ Day 1/hello_world.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Includes the input/output library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Prints text and a new line
+    return 0; // Signals to the OS that the program ran successfully
+}
