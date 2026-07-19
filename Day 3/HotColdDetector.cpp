2a46ff31a74bcf1b93403db1b03d1f65774d892d
Omar Itani <omar.itaniau@gmail.com>
2026-07-19 20:55:35 +1000

    Day 3: Hot and Cold Logic Program

---
+++ Day 3/HotColdDetector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int celsius = 35;
+
+    if (celsius > 30) {
+        std::cout << "HOT!" << std::endl;
+    } else {
+        std::cout << "COLD" << std::endl;
+    }
+}
